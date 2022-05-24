52f9173e1fa36008dedb1959d1ab9a73775dd4ad
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-05-24 11:31:06 +0530

    [jayesh] insert item in stack

---
+++ StackProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackProject
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Inserting an element");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+
+        }
+    }
+}
